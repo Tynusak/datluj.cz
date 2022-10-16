df045a352fd79b3c175bd94a9071c0583f4de8c6
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-10-16 13:58:43 +0200

    fáze 1

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
-  
-  return (
-    <div className="wordbox">{lettersLeft}</div>
-  );
+  const [lettersLeft, setLettersLeft] = useState(word);
+
+  const handleKeyUp = (e) => {
+    if (e.key === lettersLeft.charAt(0)) {
+      setLettersLeft(lettersLeft.slice(1));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyUp);
+    return () => document.removeEventListener('keyup', handleKeyUp);
+  }, [lettersLeft]);
+
+  return <div className="wordbox">{lettersLeft}</div>;
 };
 
 export default Wordbox;
